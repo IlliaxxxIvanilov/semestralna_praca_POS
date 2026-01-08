d25e606e7755c019b21ac730b22a2bf1a3c515ec
vladoReal1 <tomashulina96@gmail.com>
2026-01-08 19:36:28 +0100

    upravaHlavKodMutex

--- server/networking/message.c
+++ server/networking/message.c
@@ -1,5 +1,5 @@
 #include "message.h"
-#include "../shared/protocol.h"
+#include "../../shared/protocol.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
--- server/parking/vehicle.c
+++ server/parking/vehicle.c
@@ -1,12 +1,24 @@
 #include "vehicle.h"
 #include "../utils/random_generator.h"
 #include <time.h>
+#include <pthread.h>
 
+/*
+ * Globálny čítač ID vozidiel.
+ * Chránený mutexom, aby bol thread-safe.
+ */
 static int vehicle_id_counter = 0;
+static pthread_mutex_t vehicle_id_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void vehicle_init(vehicle_t *v, const sim_config_t *cfg) {
+    /* --- THREAD-SAFE generovanie ID --- */
+    pthread_mutex_lock(&vehicle_id_mutex);
     v->id = vehicle_id_counter++;
+    pthread_mutex_unlock(&vehicle_id_mutex);
+
+    /* --- Časy vozidla --- */
     v->arrival_time = time(NULL);
     v->park_duration = random_generator_int(cfg->park_min, cfg->park_max);
     v->departure_time = v->arrival_time + v->park_duration;
 }
+
