5d5587b6c069b940580f34493dff960d4788cff5
vladoReal1 <tomashulina96@gmail.com>
2026-01-09 13:00:02 +0100

    +add pridanie do clienta subrov na vypis  parkoviska a pohlada nanhno

---
+++ client/ui/parking_view.h
@@ -0,0 +1,12 @@
+#ifndef PARKING_VIEW_H
+#define PARKING_VIEW_H
+
+#include "../../shared/dto.h"
+
+/*
+ * Vypíše aktuálny stav parkoviska
+ */
+void parking_view_print(const parking_state_t *state);
+
+#endif
+
