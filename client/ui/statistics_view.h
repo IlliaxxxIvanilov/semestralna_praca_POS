ad7e4aa9614880bc91b19ebdf3b9e2b5ba937685
IlliaxxxIvanilov <illia.ivanilov04@gmail.com>
2026-01-10 19:36:09 +0100

    prerobenie projektu

--- client/ui/statistics_view.h
+++
@@ -1,9 +0,0 @@
-#ifndef STATISTICS_VIEW_H
-#define STATISTICS_VIEW_H
-
-#include "../../shared/dto.h"
-
-void statistics_view_print(const statistics_t *stats);
-
-#endif
-
--- server/parking/vehicle.h
+++
@@ -1,9 +0,0 @@
-#ifndef VEHICLE_H
-#define VEHICLE_H
-
-#include "../../shared/dto.h"
-#include "../simulation/simulation_config.h"
-
-void vehicle_init(vehicle_t *v, const sim_config_t *cfg);
-
-#endif
